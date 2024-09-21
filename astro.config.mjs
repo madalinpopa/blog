42f2601125a117b6d1d78fc7bcaf58626d1c8bd2
Madalin Popa <contact@madalinpopa.com>
2024-09-21 16:48:45 +0300

    Refactor Astro config to include sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,9 @@ import alpine from "@astrojs/alpinejs";
 
 import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpine(), icon()],
+  integrations: [tailwind(), alpine(), icon(), sitemap()],
 });
