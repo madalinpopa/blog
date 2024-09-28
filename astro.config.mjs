7af5cd2ee180813b88cbba4538e7f03a90f0cbbc
Madalin Popa <contact@madalinpopa.com>
2024-09-28 16:43:25 +0300

    Update Astro config for Alpine.js entrypoint and integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpine from "@astrojs/alpinejs";
-
 import icon from "astro-icon";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpine(), icon(), sitemap()],
+  integrations: [
+    tailwind(),
+    alpine({ entrypoint: "/src/entrypoint" }),
+    icon(),
+    sitemap(),
+  ],
 });
